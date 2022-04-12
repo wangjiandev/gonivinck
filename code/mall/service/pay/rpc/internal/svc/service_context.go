c4b9baa3c883ae5c53a192a45f2512a7b92fdb5d
wangjian <wangjian0504@gmail.com>
2022-04-13 01:21:25 +0800

    完成基本逻辑

--- code/mall/service/pay/rpc/internal/svc/service_context.go
+++ code/mall/service/pay/rpc/internal/svc/service_context.go
@@ -1,13 +1,28 @@
 package svc
 
-import "mall/service/pay/rpc/internal/config"
+import (
+	"mall/service/order/rpc/order"
+	"mall/service/pay/model"
+	"mall/service/pay/rpc/internal/config"
+	"mall/service/user/rpc/user"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config   config.Config
+	PayModel model.PayModel
+	UserRpc  user.User
+	OrderRpc order.Order
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
 	return &ServiceContext{
-		Config: c,
+		Config:   c,
+		PayModel: model.NewPayModel(conn, c.CacheRedis),
+		UserRpc:  user.NewUser(zrpc.MustNewClient(c.UserRpc)),
+		OrderRpc: order.NewOrder(zrpc.MustNewClient(c.OrderRpc)),
 	}
 }
